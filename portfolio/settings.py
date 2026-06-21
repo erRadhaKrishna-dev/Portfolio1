20eb84233abe1f9e1a2d7de828b8889966ec0d6f
Radha Krishna <krish.ra10.20@gmail.com>
2026-06-21 16:17:43 +0530

    Render deployment setup

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -131,7 +131,7 @@ STATIC_URL = '/static/'
 STATIC_DIRS=BASE_DIR/"static"
 STATICFILES_DIRS=[STATIC_DIRS]
 STATIC_ROOT=os.path.join(BASE_DIR,"staticfiles")
-
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
